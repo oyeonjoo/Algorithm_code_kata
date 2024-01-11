8a163b99ccaa52f6e104ceeb0467ca2b416d68aa
oyeonjoo <109337974+oyeonjoo@users.noreply.github.com>
2024-01-11 10:21:24 +0900

    [level unrated] Title: 각도기, Time: 0.01 ms, Memory: 3.66 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120829. 각도기/각도기.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int angle) {
+    int result = 0;
+    
+    if(angle < 90) {
+        result = 1;
+    } else if(angle == 90) {
+        result = 2;
+    } else if(angle < 180) {
+        result = 3;
+    } else if(angle == 180) {
+        result = 4;
+    } return result;
+}
